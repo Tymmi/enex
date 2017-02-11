cf54cd29335bad1e5a768af58cb34df971b757ce
Tim <tim_jannik@gmx.de>
2017-02-11 12:56:29 +0100

    bug fixes

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,3 +1,5 @@
+
+
 'use strict';
 
 var myAngularModule = angular.module('myApp', ['ngRoute']);
@@ -17,23 +19,35 @@ myAngularModule.controller('myCtrl', function($scope, dataService) {
         })
         });
 
-myAngularModule.config(['$httpProvider' , '$routeProvider', '$locationProvider', function($httpProvider, $routeProvider, $locationProvider, $dataServiceProvider) {
+myAngularModule.config(['$httpProvider' , '$routeProvider', '$locationProvider', function($httpProvider, $routeProvider, $locationProvider) {
   //Enable cross domain calls
   $httpProvider.defaults.useXDomain = true;
   $routeProvider.otherwise({redirectTo: '/view1'});
   $locationProvider.hashPrefix('!');
 }]);
 
-myAngularModule.service('myService', function($http) {
+myAngularModule.service('dataService', function($http) {
     delete $http.defaults.headers.common['X-Requested-With'];
     this.getData = function() {
         return $http({
             method: 'GET',
             url: 'http://eve-marketdata.com/api/',
-            data: JSON.stringify(response),
+            data: JSON.stringify(dataService),
             params: 'limit=10, sort_by=created:desc',
             withCredentials: true,
             headers: {'Authorization': 'Basic bashe64usename:password'}
         })
     }
+});
+
+// Simple GET request example:
+$http({
+    method: 'GET',
+    url: '/someUrl'
+}).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+}, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
 });
